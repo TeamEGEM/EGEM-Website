58cecd9a2d89aec991aa7e3a734e4f8a9a096be1
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-11-21 12:26:16 -0800

    small and big additions!

--- js/egemnode.js
+++ js/egemnode.js
@@ -1,1 +1,21 @@
-var getJSON = require('get-json');
+// Fade in logo, and social media links
+$(document).ready(function () {
+    $('.egem-logo').delay(500).fadeIn(1000);
+    $('.social-media').delay(750).fadeIn(1000);
+    $('#prmapimg').delay(1750).fadeIn(1000).fadeOut(500).fadeIn(1000).fadeOut(500).fadeIn(1000);
+    $("#rmapdiv").hide();
+    $("#prcme").hide();
+    $("#prmap").click(function(){
+        $("#prcme").slideToggle('fast');
+        $("#prome").slideToggle('fast');
+        $("#rmapdiv").slideToggle(3000);
+    });
+    $('#faqimg').delay(2750).fadeIn(1000).fadeOut(500).fadeIn(1000).fadeOut(500).fadeIn(1000);
+    $("#faqdiv").hide();
+    $("#faqcme").hide();
+    $("#faq").click(function(){
+        $("#faqcme").slideToggle('fast');
+        $("#faqome").slideToggle('fast');
+        $("#faqdiv").slideToggle(3000);
+    });
+});
--- js/functions.js
+++ js/functions.js
@@ -8,20 +8,6 @@ $(document).ready(function () {
     });
 });
 
-// Fade in logo, and social media links
-$(document).ready(function () {
-    $('.egem-logo').delay(500).fadeIn(1000);
-    $('.social-media').delay(750).fadeIn(1000);
-    $('#prmapimg').delay(1750).fadeIn(1000).fadeOut(500).fadeIn(1000).fadeOut(500).fadeIn(1000);
-    $("#rmapdiv").hide();
-    $("#prcme").hide();
-    $("#prmap").click(function(){
-        $("#prcme").slideToggle('fast');
-        $("#prome").slideToggle('fast');
-        $("#rmapdiv").slideToggle(3000);
-    });
-});
-
 // Show mobile nav when clicking on hamburger and hide when a link is selected
 $(document).ready(function () {
     $('hamburger-menu').add('.mobile-nav').on('click', function () {
